244ce5227b5e173be196d7ee1bfef1c58f85c8ba
Reshetov Yuriy <45720387+Yurback@users.noreply.github.com>
2024-02-26 19:37:44 +0300

    11 issue done

--- project_euler.js
+++ project_euler.js
@@ -187,7 +187,7 @@ $$(1 + 2 + ... + 10)^2 = 55^2 = 3025.$$
 // 8 +++++++++++++++++++++++++++++++++++++++++++++
 // Largest Product in a Series
 
-let str =`73167176531330624919225119674426574742355349194934
+let str = `73167176531330624919225119674426574742355349194934
 96983520312774506326239578318016984801869478851843
 85861560789112949495459501737958331952853208805511
 12540698747158523863050715693290963295227443043557
@@ -208,8 +208,8 @@ let str =`73167176531330624919225119674426574742355349194934
 05886116467109405077541002256983155200055935729725
 71636269561882670428252483600823257530420752963450`.split('\n').join('');
 
-        // The four adjacent digits in the 1000 
-        // -digit number that have the greatest product are 9*9*8*9=5832
+// The four adjacent digits in the 1000 
+// -digit number that have the greatest product are 9*9*8*9=5832
 
 //  Find the thirteen adjacent digits in the 13
 // -digit number that have the greatest product. What is the value of this product?
@@ -236,14 +236,14 @@ let str =`73167176531330624919225119674426574742355349194934
 //         }
 //  }
 //  console.log(findadjecents(13));
- 
+
 // 9 +++++++++++++++++++++++++++++++++++++++++++++
 // Special Pythagorean Triplet
 /* <p>A Pythagorean triplet is a set of three natural numbers, $a \lt b \lt c$, for which,
 $$a^2 + b^2 = c^2.$$</p>
 <p>For example, $3^2 + 4^2 = 9 + 16 = 25 = 5^2$.</p>
 <p>There exists exactly one Pythagorean triplet for which $a + b + c = 1000$.<br>Find the product $abc$.</p> */
- 
+
 // function check(a,b,c) {
 //         if ((a**2 + b**2)==c**2 && (a+b+c)==1000) return true;
 //         return false;
@@ -274,32 +274,111 @@ $$a^2 + b^2 = c^2.$$</p>
 //  is 2+3+5+7=17
 // Find the sum of all the primes below two million.
 
-function isPrime(num) {
-    const pivot = Math.sqrt(num);
-    for (let i = 2; i <= pivot; i++) {
-        if (num % i === 0) return false;
-    }
-    return true;
-}
+// function isPrime(num) {
+//     const pivot = Math.sqrt(num);
+//     for (let i = 2; i <= pivot; i++) {
+//         if (num % i === 0) return false;
+//     }
+//     return true;
+// }
 
-function findPrime(total) {
-    const arrPrime = [2];
-    let num = 3;
-    let pos = 0;
-    while (arrPrime[pos]<2000000) {
-        if (isPrime(num)) {
-            arrPrime.push(num);
-            num++;
-            pos++;
-        } else {
-            num++
+// function findPrime(total) {
+//     const arrPrime = [2];
+//     let num = 3;
+//     let pos = 0;
+//     while (arrPrime[pos]<2000000) {
+//         if (isPrime(num)) {
+//             arrPrime.push(num);
+//             num++;
+//             pos++;
+//         } else {
+//             num++
+//         }
+//     }
+//     console.log(arrPrime);
+//     console.log(arrPrime[pos]);
+//     arrPrime.pop();
+//     console.log(arrPrime.reduce((sum,element)=>sum+element));
+
+// }
+
+// findPrime();
+
+// 10 +++++++++++++++++++++++++++++++++++++++++++++
+// Largest Product in a Grid
+
+/* <p>The product of these numbers is $26 \times 63 \times 78 \times 14 = 1788696$.</p>
+<p>What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the $20 \times 20$ grid?</p> */
+
+let matrix = [
+        [08,02, 22, 97, 38, 15,00, 40,00, 75,04,05,07, 78, 52, 12, 50, 77, 91,08],
+        [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48,04, 56, 62,00],
+        [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30,03, 49, 13, 36, 65],
+        [52, 70, 95, 23,04, 60, 11, 42, 69, 24, 68, 56,01, 32, 56, 71, 37,02, 36, 91],
+        [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+        [24, 47, 32, 60, 99,03, 45,02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+        [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+        [67, 26, 20, 68,02, 62, 12, 20, 95, 63, 94, 39, 63,08, 40, 91, 66, 49, 94, 21],
+        [24, 55, 58,05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+        [21, 36, 23,09, 75,00, 76, 44, 20, 45, 35, 14,00, 61, 33, 97, 34, 31, 33, 95],
+        [78, 17, 53, 28, 22, 75, 31, 67, 15, 94,03, 80,04, 62, 16, 14,09, 53, 56, 92],
+        [16, 39,05, 42, 96, 35, 31, 47, 55, 58, 88, 24,00, 17, 54, 24, 36, 29, 85, 57],
+        [86, 56,00, 48, 35, 71, 89,07,05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+        [19, 80, 81, 68,05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77,04, 89, 55, 40],
+        [04, 52,08, 83, 97, 35, 99, 16,07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+        [88, 36, 68, 87, 57, 62, 20, 72,03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+        [04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18,08, 46, 29, 32, 40, 62, 76, 36],
+        [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74,04, 36, 16],
+        [20, 73, 35, 29, 78, 31, 90,01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57,05, 54],
+        [01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52,01, 89, 19, 67, 48]
+];
+
+function findProduct(matrix) {
+        let maxproduct = 0;
+        let obj = {};
+        // diagonal check 1
+        for (let i = 0; i < matrix.length - 3; i++) {
+                for (let j = 0; j < matrix[i].length - 3; j++) {
+                        let product = matrix[i][j] * matrix[i + 1][j + 1] * matrix[i + 2][j + 2] * matrix[i + 3][j + 3];
+                        if (product > maxproduct) {
+                                maxproduct = product;
+                                obj = { 1: matrix[i][j], 2: matrix[i + 1][j + 1], 3: matrix[i + 2][j + 2], 4: matrix[i + 3][j + 3], direction: "d1" };
+                        }
+                }
         }
-    }
-    console.log(arrPrime);
-    console.log(arrPrime[pos]);
-    arrPrime.pop();
-    console.log(arrPrime.reduce((sum,element)=>sum+element));
+        // diagonal check 2
+        for (let i = 0; i < matrix.length - 3; i++) {
+                for (let j = 3; j < matrix[i].length; j++) {
+                        let product = matrix[i][j] * matrix[i + 1][j - 1] * matrix[i + 2][j - 2] * matrix[i + 3][j - 3];
+                        if (product > maxproduct) {
+                                maxproduct = product;
+                                obj = { 1: matrix[i][j], 2: matrix[i + 1][j - 1], 3: matrix[i + 2][j - 2], 4: matrix[i + 3][j - 3], direction: "d2" };
+                        }
+                }
+        }
+        // gorisontal check
+        for (let i = 0; i < matrix.length; i++) {
+                for (let j = 0; j < matrix[i].length - 3; j++) {
+                        let product = matrix[i][j] * matrix[i][j + 1] * matrix[i][j + 2] * matrix[i][j + 3];
+                        if (product > maxproduct) {
+                                maxproduct = product;
+                                obj = { 1: matrix[i][j], 2: matrix[i][j + 1], 3: matrix[i][j + 2], 4: matrix[i][j + 3], direction: "gorisont" };
+                        }
+                }
+        }
+        // vertical check
+        for (let i = 0; i < matrix.length-3; i++) {
+                for (let j = 0; j < matrix[i].length; j++) {
+                        let product = matrix[i][j] * matrix[i+1][j] * matrix[i+2][j] * matrix[i+3][j];
+                        if (product > maxproduct) {
+                                maxproduct = product;
+                                obj = { 1: matrix[i][j], 2: matrix[i+1][j], 3: matrix[i+2][j], 4: matrix[i+3][j], direction: "vertical"};
+                        }
+                }
+        }
 
-}
+        console.log(maxproduct);
+        console.log(obj);
+};
 
-findPrime();
+findProduct(matrix);
